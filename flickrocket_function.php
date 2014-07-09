9e783dc95c5c387364119785f91465b4583cdf1b
FlickRocket <FlickRocket@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-09 15:24:56 +0000

    First Release
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-digital-content-delivery-with-drm-flickrocket/trunk@945674 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ flickrocket_function.php
@@ -0,0 +1,170 @@
+<?php 	
+class Flickrocket
+{
+	private $post_errors 	= array();
+	public $admin_email, $admin_pass, $theme_id, $email, $pass, $transactionID, $projectID, $license, $count, $sandbox_active;
+	public $flick_rocket_admin_email, $flick_rocket_admin_password, $flick_rocket_theme_id, $soapobj, $webservice_url;
+	
+	//check live or sandbox flickrocket api
+	public function __construct(){
+		$useSandbox = get_option( 'sandbox_active', false );
+		
+		if ($useSandbox == 'yes'){ 
+			$webservice_url = 'http://sandbox.flickrocket.com/Services/OnDemandOrder/service.asmx?WSDL';
+		
+		}else{ 
+			$webservice_url = 'https://www.flickrocket.com/Services/OnDemandOrder/service.asmx?WSDL'; 
+		}
+							
+		$this->soapobj 		= new SoapClient($webservice_url, array('trace' => true));
+	}
+	
+	//check flickrocket account
+	public function flickRocketCheckAccount(){
+				
+		$param 				= array();
+		$param['EMail'] 	= $this->flick_rocket_admin_email;
+		$param['Password'] 	= $this->flick_rocket_admin_password;
+		$param['ThemeID'] 	= $this->flick_rocket_theme_id;
+				
+		if($this->sandbox_active == 'true')
+			$webservice_url_chk = 'http://sandbox.flickrocket.com/Services/OnDemandOrder/service.asmx?WSDL';
+		else
+			$webservice_url_chk	= 'https://www.flickrocket.com/Services/OnDemandOrder/service.asmx?WSDL';
+			
+						
+		$soapobjchk 			= new SoapClient($webservice_url_chk, array('trace' => true));
+		$ret 					= $soapobjchk->CheckAccount($param);
+				
+		if (!empty($ret))
+			return $ret->ErrorCode;
+		else
+			return 1;
+	}
+	
+	//check flickrocket user exists 
+	public function flickRocketCheckUserExists( $flickRAdminEmail, $flickRAdminPass, $flickRUserEmail, $flickRUserPSW ){
+			
+		$param 				= array();
+		$param['EMail'] 	= $flickRAdminEmail; 
+		$param['Password'] 	= $flickRAdminPass;
+		$param['XML'] 		= '<?xml version="1.0" encoding="utf-8" ?>
+								<Customer>
+									<EMail>'.$flickRUserEmail.'</EMail>
+									<Password>'.$flickRUserPSW.'</Password>
+								</Customer>';
+		return $ret = $this->soapobj->CheckUserExists($param);
+	}
+	
+	//check flickrocket user data
+	public function flickRocketPrepareLoginEx( $admin_email, $admin_pass, $theme_id, $email, $pass ){
+	
+		$param 				= array();
+		$param['EMail'] 	= $admin_email;
+		$param['Password'] 	= $admin_pass;
+		$param['ThemeID'] 	= $theme_id;
+		$param['XML'] 		= '<?xml version="1.0" encoding="utf-8" ?>
+								<Customer>
+									<EMail>'.$email.'</EMail>
+									<Password>'.$pass.'</Password>
+								</Customer>';
+		
+		return $ret = $this->soapobj->PrepareLoginEx($param);
+	}
+	
+	//create shop on flickrocket
+	public function flickRocketCreateShopOrder( $admin_email, $admin_pass, $theme_id, $email, $pass, $transactionID, $projectID, $license, $count, $xmlString ){
+	
+		$param 				= array();
+		$param['EMail'] 	= $admin_email;
+		$param['Password'] 	= $admin_pass;
+		$param['XML'] 		= '<?xml version="1.0" encoding="utf-8" ?>
+								<Order>
+									<TransactionID>'.$transactionID.'</TransactionID>
+									<EMail>'.$email.'</EMail>
+									<Password>'.$pass.'</Password>
+									<ThemeID>'.$theme_id.'</ThemeID>
+									<Items>
+										'.$xmlString.'
+									</Items>
+								</Order>';
+		
+		return $ret = $this->soapobj->CreateShopOrder($param);
+	}
+	
+	//change user password 
+	public function flickRocketChangePassword( $admin_email, $admin_pass, $theme_id, $email, $pass, $newpass ){
+	
+		$param 				= array();
+		$param['EMail'] 	= $admin_email;
+		$param['Password'] 	= $admin_pass;
+		$param['XML'] 		= '<?xml version="1.0" encoding="utf-8" ?>
+								<Customer>
+									<EMail>'.$email.'</EMail>
+									<Password>'.$pass.'</Password>
+									<NewPassword>'.$newpass.'</NewPassword>
+								</Customer>';
+		$ret = $this->soapobj->ChangeCustomerPassword($param);
+		return $ret;
+	}
+	
+	//change user email address 
+	public function flickRocketChangeEmail( $admin_email, $admin_pass, $theme_id, $email, $pass, $newemail ){
+	
+		$param 				= array();
+		$param['EMail'] 	= $admin_email;
+		$param['Password'] 	= $admin_pass;
+		$param['XML'] 		= '<?xml version="1.0" encoding="utf-8" ?>
+								<Customer>
+									<EMail>'.$email.'</EMail>
+									<NewEMail>'.$newemail.'</NewEMail>
+									<Password>'.$pass.'</Password>
+								</Customer>';
+								
+		$ret = $this->soapobj->ChangeCustomerEmail($param);
+		return $ret;
+	}
+	
+	//get projects list select box from flickrocket api
+	public function flickRocketGetProjects($fieldName, $selectedValue){
+		
+		$FRObj				= new FlickRocketWooocommerce();
+		$config 			= $FRObj::get_flickrocket_config_data();		
+		
+		$param 				= array();
+		$param['EMail'] 	= $config['flickrocket_user_email'];
+		$param['Password'] 	= $config['flickrocket_user_password'];
+		$param['ThemeID'] 	= $config['flickrocket_theme_id'];
+		
+		$fieldsValue 		= $this->soapobj->GetProjects($param);
+		$selectBox = '';
+		
+		$selectBox = '<select name="' . $fieldName . '"><option value=""> -- Select Project ID -- </option>';
+		
+		foreach($fieldsValue->Projects->stProject as $sbValue){
+			$selectedField = $sbValue->LongProjectID == $selectedValue ? 'selected' : '';
+			$selectBox .= '<option value="' . $sbValue->LongProjectID . '" ' . $selectedField . '>' . $sbValue->Name . ' (' . $sbValue->LongProjectID . ') </option>';
+		}
+		
+		$selectBox .= '</select>';
+		
+		return $selectBox;
+	}
+	
+	//get flickrocket license data
+	public function flickRocketGetLicenses(){
+		
+		$FRObj				= new FlickRocketWooocommerce();
+		$config 			= $FRObj::get_flickrocket_config_data();		
+		
+		$param 				= array();
+		$param['EMail'] 	= $config['flickrocket_user_email'];
+		$param['Password'] 	= $config['flickrocket_user_password'];
+		
+		$fieldsValue 		= $this->soapobj->GetLicenses($param);
+				
+		return $fieldsValue;
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,85 @@
+=== Plugin Name ===
+Contributors: FlickRocket
+Donate link: http://flickrocket.com/
+Tags: video, audio, ebook, DRM, content, digital rights management, apps, iOS, Android, Windows, MacOSX, Kindle, SmartTV
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Sales and rentals of (optionally DRM protected) digital content such as DVDs, video (HD+SD), audio books, ebooks (epub and PDF) and packaged content
+
+== Description ==
+
+#Sell and rent DRM protected digital content (DVD, video, audio, ebooks and content packages)#
+
+This extension enables you to sell and rent (optionally DRM protected) digital content such as DVDs (incl. all menus, bonus material, etc.), video (HD+SD), audio books, ebooks (epub and PDF) and packaged content such as HTML, Flash, images, etc.
+
+##Windows, MacOSX, iOS, Android, Kindle and SmartTV##
+
+Customers can consume the content on virtually all platforms and access the content also on multiple devices. While doing so the content is transparently end-to-end protected and you can freely define usage time frames, device limitations, regional limitations, and much more.
+
+##Everything included for getting started##
+
+You can encode, package, encrypt and upload your content right within WooCommerce/Wordpress or use desktop software (free download for Windows and Mac) to get your content ready for sale. The content distribution is done via our content delivery network (CDN) to ensure a high bandwidth distribution to a world wide audience.
+
+##More features for content stores##
+
+This extension includes not only the backend features to set up your content but also shop frontend features so you can present trailers and about guiding people after the checkout. It also includes a digital locker for customer to log in and access their purchased content at a later time.
+
+##Easy to use DRM control##
+
+You don’t need to be an expert in DRM to get your content sales up can running. DRM is applied automatically during upload and you can select from various pre-defined licenses. You can even offer customers the option to select between licenses (e.g. download-to-own or rental) at different prices.
+
+== Installation ==
+
+1. Make sure Wordpress and WooCommerce are installed
+2. Go to Plug-ins -> New Plug-in and install the plug-in
+3. Activate the plug-in
+
+== Frequently Asked Questions ==
+
+= Can the plug-in work just with Wordpress but without WooCommerce? =
+
+No, WooCommerce is required.
+
+= Do I need a FlickRocket account to use the plug-in? =
+
+You can test the plug-in by using the pre-installed Sandbox account. However, for productiove use you need to sign up to FlickRocket.
+
+= Is there any cost for using the plug-in =
+
+The plug-in is provided free. If you want to use it, you need to sign up to a FlickRocket account. If you go with the free BASIC account there is no cost. There is a cost for the PREMIUM accouts.
+
+= Which kind of content is supported by FlickRocket? =
+
+Unfortunately the space here is not enough to list all kinds of supported content. Some examples are availabel on the FlicRrocket web site. If you are unsure, just contact us.
+
+= Can the content be cosumed on every platform =
+
+At time of this wrting we support Windows, MacOSX, iOS, Android, Kindle, SmartTV and more.
+
+== Screenshots ==
+
+1. Shop view in sample theme
+2. Product details view with license selection (optional)
+3. Button for Digital Cntent access after purchase complete
+4. Customizable player installation page (active OS on top)
+5. iPad content overview - tile mode (landscape)
+6. iPad content overview - list mode (landscape)
+7. iPad during video play (landscape)
+8. iPad during video play with DVD navigation (landscape)
+9. iPhone content overview - tile mode (portrait)
+10. iPhone content overview - list mode (portrait)
+
+== Changelog ==
+
+= 1.0 =
+* Public release version
+
+== Upgrade Notice ==
+
+= 1.0 =
+Public release version
+
